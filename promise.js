641ecdc397f5d4140489efa57400f4f64c03ee0e
Julie Hu <jhu21495@gmail.com>
2020-11-16 16:48:56 -0800

    promise

--- promise.js
+++ promise.js
@@ -1,7 +1,7 @@
 let p = new Promise((resolve,reject) => {
     let a = 1 + 1 
     if (a ==2) {
-        resolve('Sucess')
+        resolve('Success')
     } else {
         reject('Failed')
     }  
@@ -11,4 +11,96 @@ p.then((message) => {
     console.log('This is in the then' + message)
 }).catch((message) => {
     console.log('This is in the catch' + message)
+})
+
+
+function watchTutorialCallback(callback, errorCallback) {
+  let userLeft = false
+  let userWatchingCatMeme = false
+
+  if (userLeft) {
+    errorCallback({
+      name: 'User Left', 
+      message: ':('
+    })
+  } else if (userWatchingCatMeme) {
+    errorCallback({
+      name: 'User Watching Cat Meme',
+      message: 'WebDevSimplified < Cat' 
+    })
+  } else {
+    callback('Thumbs up and Subscribe')
+  }
+}
+
+// change watchtutorialcallback into a promise
+// promises dont take parameters
+
+function watchTutorialPromise() {
+  let userLeft = false
+  let userWatchingCatMeme = false
+  return new Promise((resolve, reject) => {
+    if (userLeft) {
+      reject({
+        name: 'User Left', 
+        message: ':('
+      })
+    } else if (userWatchingCatMeme) {
+      reject({
+        name: 'User Watching Cat Meme',
+        message: 'WebDevSimplified < Cat' 
+      })
+    } else {
+      resolve('Thumbs up and Subscribe')
+    }
+  })
+}
+
+watchTutorialCallback(message => {
+  console.log(message)
+}, error => {
+  console.log(error.name + ' ' + error.message)
+})
+
+// returns promose.then and .catch
+watchTutorialPromise().then(message => {
+  console.log(message)
+}).catch(error => {
+  console.log(error.name + ' ' + error.message)
+})
+
+
+// all of these reslove and not reject
+
+const recordVideoOne = new Promise((resolve, reject) => {
+  resolve('Video 1 Recorded')
+})
+
+const recordVideoTwo = new Promise((resolve, reject) => {
+  resolve('Video 2 Recorded')
+})
+
+const recordVideoThree = new Promise((resolve, reject) => {
+  resolve('Video 3 Recorded')
+})
+
+// send array of promises ... alll of them resolve 
+// .then will print out "Video # Recorded " result in console 
+Promise.all([
+  recordVideoOne,
+  recordVideoTwo,
+  recordVideoThree
+]).then(messages => {
+  console.log(messages)
+})
+
+
+// overturns as soon as one of them finishes and returns one message as opposed to 3 
+
+Promise.race([
+  recordVideoOne,
+  recordVideoTwo,
+  recordVideoThree
+]).then(message => {
+  console.log(message)
 })
